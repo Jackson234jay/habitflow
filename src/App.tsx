7bd5376b2067dbc0a9e1fb86ff87a5cdb30ff576
Jackson234jay <blossomjohn399@gmail.com>
2026-04-05 18:46:10 +0100

    Setup + theme configuration

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-primary-content bg-primary">Hello World</h1>
+      <div role="alert" className="alert alert-error">
+        <span>Error! Task failed successfully.</span>
+      </div>
+    </div>
+  );
+};
+
+export default App;
