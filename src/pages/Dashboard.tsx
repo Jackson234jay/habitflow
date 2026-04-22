0ef694927e06e4c9408064af000409ed6dd10221
Jackson234jay <blossomjohn399@gmail.com>
2026-04-22 14:57:07 +0100

    `Added new UI elements to the Dashboard page`

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,3 +1,4 @@
+import { Plus } from "lucide-react";
 import Layout from "../components/Navbar";
 import { useAuth } from "../context/AuthContext";
 
@@ -5,13 +6,43 @@ const Dashboard = () => {
   const { user } = useAuth();
   const fullName = user?.user_metadata?.full_name || "";
   const firstName = fullName.split(" ")[0];
+  const today = new Date();
+  const formattedDate = new Intl.DateTimeFormat("en-US", {
+    weekday: "long",
+    month: "long",
+    day: "numeric",
+  }).format(today);
   console.log(user);
 
   return (
     <Layout>
-      <h1 className="text-xl font-bold">
-        Welcome {firstName || user?.email}👋
+      <h1 className="text-3xl font-bold">
+        Good morning {firstName || user?.email}👋
       </h1>
+      <div className="flex justify-between">
+        <p className="text-neutral-content">
+          {formattedDate}
+          <span className="px-1">{`\u2022`}</span>You have 2 habits left for
+          today.
+        </p>
+        <button className="btn  bg-[#C084FC] hover:bg-primary-content outline-none border-transparent flex items-center gap-2 text-white">
+          <span className="">
+            <Plus />
+          </span>
+          Add Habit
+        </button>
+      </div>
+      <div className="py-4">
+        <div className="card bg-base-100">
+          <div className="card-body">
+            <h2 className="card-title">Card title!</h2>
+            <p>
+              A card component has a figure, a body part, and inside body there
+              are title and actions parts
+            </p>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
