6fc8f4ba236e1a6bdbb87f8d65d62c4c91718713
Jackson234jay <blossomjohn399@gmail.com>
2026-04-22 11:40:02 +0100

    `Update Navbar component styles and layout`

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,21 +10,22 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       {/* SIDEBAR */}
       <div
         className={`
-          fixed md:static top-0 left-0 h-screen w-64 bg-base-100 shadow-sm z-50
-          transform transition-transform duration-300
-          ${open ? "translate-x-0" : "-translate-x-full md:translate-x-0"}
-        `}
+    fixed md:static top-0 left-0 h-screen w-64 bg-base-100 shadow-sm z-50
+    border-r border-r-neutral 
+    transform transition-transform duration-300
+    ${open ? "translate-x-0" : "-translate-x-full md:translate-x-0"}
+  `}
       >
-        <div className="p-4 flex items-center gap-3 font-bold bg-base-100">
+        <div className="p-4 flex items-center gap-3  font-bold bg-base-100">
           <span className="w-8 h-8 rounded-xl text-sm shadow-lg shadow-primary flex bg-primary-content text-white items-center justify-center">
             H
           </span>
           HabitFlow
         </div>
 
-        <nav className="p-4 space-y-3">
-          <a className="block p-2 hover:bg-base-200 rounded">Home</a>
-          <a className="block p-2 hover:bg-base-200 rounded">Dashboard</a>
+        <nav className="p-4  space-y-3">
+          <button className="block p-2 hover:bg-base-200 rounded">Home</bu>
+          <buttoclassName="block p-2 hover:bg-base-200 rounded">Dashboard</buttoclassName=>
           <a className="block p-2 hover:bg-base-200 rounded">Settings</a>
         </nav>
       </div>
@@ -32,7 +33,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       {/* RIGHT SIDE (TOPBAR + CONTENT) */}
       <div className="flex-1 flex flex-col">
         {/* TOPBAR */}
-        <div className="flex items-center justify-between p-4 border-b bg-base-200">
+        <div className="flex items-center justify-between p-4 border-b border-b-neutral bg-base-200">
           {/* Mobile menu button */}
           <button className="md:hidden" onClick={() => setOpen(true)}>
             <Menu />
